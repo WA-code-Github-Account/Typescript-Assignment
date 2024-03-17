1d09acdd0bc60e43a78dd223b49a6e6b7f55d82d
WA-code-Github-Account <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-03-17 14:28:26 -0700

    Delete Famouse_Quotes_2.ts

--- Famouse_Quotes_2.ts
+++
@@ -1,5 +0,0 @@
-let famouse_person: string ='Albert Einstein';
-
-let message: string = `"I have no special talent, I am only passionately curoious"`;
-
-console.log(`${famouse_person} once said, ${message}`);
